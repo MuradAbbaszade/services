dcd86b3dc6be9af7cb217c5829eb1aecd4f9ed5b
abbaszade <muradabbaszade6@gmail.com>
2022-12-09 17:33:55 +0400

    Update admin panel

---
+++ src/main/java/world/rfch/controller/AuthController.java
@@ -0,0 +1,13 @@
+package world.rfch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/adminLogin")
+    public String showAdminLoginPage(){
+        return "login.html";
+    }
+}
